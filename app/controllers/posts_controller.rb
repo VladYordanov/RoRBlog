de9ec254a9e46bd92dbb9d4fdb0594637001f036
VladYordanov <vyordanoff@gmail.com>
2015-08-21 03:51:52 -0700

    initial commit with working add/edit/delete post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def create
+		@post = Post.new(article_params)
+		
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update 
+		@post = Post.find(params[:id])
+		
+		if @post.update(article_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
+
+	private
+		def article_params
+			params.require(:post).permit(:title, :text)
+		end
+end
